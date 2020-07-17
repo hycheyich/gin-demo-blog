3a48742292a2561c323f1216911d1626a781d478
hycheyich <18870414853@163.com>
2020-07-17 11:35:38 +0800

    add:添加获取单个文章接口

--- routers/api/v1/article.go
+++ routers/api/v1/article.go
@@ -1,12 +1,42 @@
 package v1
 
-import "github.com/gin-gonic/gin"
-
-
-
+import (
+	"github.com/EDDYCJY/go-gin-example/models"
+	"github.com/EDDYCJY/go-gin-example/pkg/e"
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+	"log"
+	"net/http"
+)
 
 // 获取单个文章
 func GetArticle(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("ID必须大于0")
+	code := e.INVALID_PARAMS
+	var data interface{}
+	if !valid.HasErrors() {
+		//	 表示校验通过
+		if models.ExistArticleByID(id) {
+			//	表示存在ID的文章
+			data = models.GetArticle(id)
+			code = e.SUCCESS
+		} else {
+			code = e.ERROR_NOT_EXIST_ARTICLE
+		}
+	} else {
+		//	表示存在错误
+		for _, err := range valid.Errors {
+			log.Printf("err.key: %s, err.message: %s", err.Key, err.Message)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code":    code,
+		"message": e.GetMsg(code),
+		"data":    data,
+	})
 
 }
 
